94d300726c3cc337900fef2093dbd21fa52fcaa6
Nick Sonner <nicksonner@gmail.com>
2018-04-03 17:06:20 +1000

    added comments

--- main.go
+++ main.go
@@ -10,13 +10,15 @@ import (
 )
 
 func main() {
-
+	// creates a new router
 	router := mux.NewRouter().StrictSlash(true)
+	// adds a "/" route, assigns the Index handler when the endpoint is called
 	router.HandleFunc("/", Index)
+	// spin up a server on port 8080, and can be accessed at http://localhost:8080
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-// Index ...
+// Index handler
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
