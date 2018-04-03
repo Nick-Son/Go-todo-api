85707336797b6f2738d797649d4cfc3e2e13d690
Nick Sonner <nicksonner@gmail.com>
2018-04-03 17:18:57 +1000

    added routes for TodoIndex, TodoShow and About

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	// adds a "/" route, assigns the Index handler when the endpoint is called
 	router.HandleFunc("/", Index)
+	// routes for Todo index, Todo show and an about page
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
+	router.HandleFunc("/about", About)
 	// spin up a server on port 8080, and can be accessed at http://localhost:8080
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
@@ -21,4 +25,21 @@ func main() {
 // Index handler
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+// TodoIndex handler
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Todo Index")
+}
+
+// TodoShow handler
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Show Todos:", todoId)
+}
+
+// About handler
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a simple RESTful todo api built with Go :)")
 }
