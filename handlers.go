6ea208de39c59c36104b6519889595984a60b820
Nick Sonner <nicksonner@gmail.com>
2018-04-05 20:06:34 +1000

    added RepoCreateTodo function

--- handlers.go
+++ handlers.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"html"
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -37,4 +39,32 @@ func TodoShow(w http.ResponseWriter, r *http.Request) {
 // About handler
 func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "This is a simple RESTful todo api built with Go :)")
+}
+
+// TodoCreate handler
+func TodoCreate(w http.ResponseWriter, r *http.Request) {
+	var todo Todo
+	// open and read the content of body, limiting the amount of data to 1048576 bytes
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+	// unmarshal the body to our todo struct
+	if err := json.Unmarshal(body, &todo); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422)
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
+	t := RepoCreateTodo(todo)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(t); err != nil {
+		panic(err)
+	}
 }
